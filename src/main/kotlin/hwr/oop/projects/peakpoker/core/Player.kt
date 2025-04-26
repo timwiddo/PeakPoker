d3c43ffa91ab83232be85a27fc95a98d0db67a22
Julian Kücken <101923513+juliankuecken@users.noreply.github.com>
2025-04-27 00:59:46 +0200

    Feat/player object (#13)
    Co-authored-by: Julian Kücken <julian.kuecken@icloud.com>
    Co-authored-by: Louis Kauer <75489200+dev-laky@users.noreply.github.com>
    Co-authored-by: Louis Kauer <laky.dev@proton.me>

--- src/main/kotlin/hwr/oop/projects/peakpoker/core/Player.kt
+++ src/main/kotlin/hwr/oop/projects/peakpoker/core/Player.kt
@@ -2,6 +2,43 @@ package hwr.oop.projects.peakpoker.core
 
 class Player(
     val name: String,
+    private var chips: Int = 0,
+    private var hand: List<Card> = emptyList(),
+    private var bet: Int = 0,
+    private var isFolded: Boolean = false,
+    private var isAllIn: Boolean = false
 ){
-// TODO: Player needs to be implemented
+    val currentBet: Int get() = bet
+    val currentChips: Int get() = chips
+    val currentHand: List<Card> get() = hand.toList()
+
+    fun raise_bet(amount: Int) {
+        if (amount < 0) {
+            throw IllegalArgumentException("Bet amount must be positive")
+        } else if (isFolded) {
+            throw IllegalStateException("Cannot raise bet after folding")
+        } else if (isAllIn) {
+            throw IllegalStateException("Cannot raise bet after going all-in")
+        }
+        bet += amount
+        chips -= amount
+    }
+
+    fun fold() {
+        isFolded = true
+    }
+
+    fun isFolded(): Boolean {
+        return isFolded
+    }
+
+    fun all_in() {
+        isAllIn = true
+    }
+
+    fun isAllIn(): Boolean {
+        return isAllIn
+    }
+
+    // TODO: Implement the hand functionality and think about it's logic
 }
--- src/test/kotlin/hwr/oop/projects/peakpoker/PlayerTest.kt
+++ src/test/kotlin/hwr/oop/projects/peakpoker/PlayerTest.kt
@@ -1,6 +1,7 @@
 package hwr.oop.projects.peakpoker
 
 import hwr.oop.projects.peakpoker.core.Player
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.AnnotationSpec
 import org.assertj.core.api.Assertions.assertThat
 
@@ -9,12 +10,58 @@ class PlayerTest : AnnotationSpec() {
     @Test
     fun `Player has name` () {
         val player = Player("Hans")
+        val playerName: String = player.name
         assertThat(player.name).isEqualTo("Hans")
     }
 
     @Test
-    fun `Player has money` () {
-    // TODO: Test needs to be implemented
+    fun `Player's bet can be raised` () {
+        val player = Player("Hans")
+        player.raise_bet(10)
+        assertThat(player.currentBet).isEqualTo(10)
+    }
+
+    @Test
+    fun `Right exception thrown on negative bet` () {
+        val player = Player("Hans")
+        val exception = shouldThrow <IllegalArgumentException> {
+            player.raise_bet(-10)
+        }
+        assertThat(exception.message).isEqualTo("Bet amount must be positive")
+    }
+
+    @Test
+    fun `Right exception on raising bet on a folded player` () {
+        val player = Player("Hans")
+        player.fold()
+        val exception = shouldThrow <IllegalStateException> {
+            player.raise_bet(100)
+        }
+        assertThat(exception.message).isEqualTo("Cannot raise bet after folding")
+    }
+
+    @Test
+    fun `Right exception on raising bet on an all-in player` () {
+        val player = Player("Hans")
+        player.all_in()
+        val exception = shouldThrow <IllegalStateException> {
+            player.raise_bet(100)
+        }
+        assertThat(exception.message).isEqualTo("Cannot raise bet after going all-in")
+    }
+
+    @Test
+    fun `Player's bet can be folded` () {
+        val player = Player("Hans")
+        player.fold()
+        assertThat(player.isFolded()).isTrue
+    }
+
+    @Test
+    fun `Player's bet can be all-in` () {
+        val player = Player("Hans")
+        player.all_in()
+        assertThat(player.isAllIn()).isTrue
     }
 
 }
