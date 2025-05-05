ee87618c6306485cb7564f8a03dacf80c7814e3b
Louis Kauer <75489200+dev-laky@users.noreply.github.com>
2025-05-05 21:25:28 +0200

    refactor: remove/queue db implementation (#28)

--- src/main/kotlin/hwr/oop/projects/peakpoker/commands/db/DbConnect.kt
+++ src/main/kotlin/hwr/oop/projects/peakpoker/commands/db/DbConnect.kt
@@ -2,8 +2,6 @@ package hwr.oop.projects.peakpoker.commands.db
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.Context
-import hwr.oop.projects.peakpoker.db.ConnectionHandler
-import hwr.oop.projects.peakpoker.db.ConnectionHandler.dbFile
 
 class DbConnect : CliktCommand(name = "connect") {
     override fun help(context: Context) = "Connect to the database."
@@ -15,7 +13,7 @@ class DbConnect : CliktCommand(name = "connect") {
         echo("Connecting to database at: ${connectionHandler.dbURL}")
         ConnectionHandler.connect()*/
 
-        echo("Connecting to database at: ${dbFile.absolutePath}")
-        ConnectionHandler.connect()
+        echo("Connecting to database at: dbFile.absolutePath")
+        // ConnectionHandler.connect()
     }
 }
